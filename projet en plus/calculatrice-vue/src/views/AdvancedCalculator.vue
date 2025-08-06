e4f495de79700c4067a95fd108810a234606c229
Euloge <mabialaeulogejunior@gmail.com>
2025-08-06 13:38:48 +0200

    supprimer le ficher projet en olus

--- projet en plus/calculatrice-vue/src/views/AdvancedCalculator.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <Calculator :mode="'advanced'" />
-</template>
-<script setup>
-import Calculator from '../components/Calculator.vue';
-</script> 
--- projet en plus/calculatrice-vue/src/views/SimpleCalculator.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <Calculator :mode="'simple'" />
-</template>
-<script setup>
-import Calculator from '../components/Calculator.vue';
-</script> 
